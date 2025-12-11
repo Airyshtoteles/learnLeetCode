62f28e7bf03d785c51804fb7cf23a7eea32afd7a
Airsytoteles <193495344+Airyshtoteles@users.noreply.github.com>
2025-12-11 20:50:25 +0700

    Day 62 Part 2: Frog Jump II

---
+++ Day62/Part2/frog_jump_ii.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    if (!(cin >> n)) return 0;
+    
+    vector<int> stones(n);
+    for(int i=0; i<n; ++i) cin >> stones[i];
+    
+    int max_jump = stones[1] - stones[0]; // Initial jump
+    
+    for(int i = 2; i < n; ++i) {
+        max_jump = max(max_jump, stones[i] - stones[i-2]);
+    }
+    
+    cout << max_jump << endl;
+    
+    return 0;
+}
